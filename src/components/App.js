0a60b29fdbb7996fad1b5c63c63f3c38fdbf192f
Sourabh Bansotiya <77042105+developer-sourabh28@users.noreply.github.com>
2024-07-25 06:44:06 +0000

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,52 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [data, setData] = useState(null);
+   const fetchUser = async() => {
+      try{
+        const response = await fetch ("https://reqres.in/api/users");
+      const data = await response.json();
+      setData(data.data);
+      }
+      catch (error) {}
+   };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+  <diV>
+    <div className="header">
+        <h2>Blue Whale</h2>
+        <button className="btn" onClick={fetchUser}>Get User List</button>
     </div>
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Avatar</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data ? (
+          data.map((user) => {
+            return (
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td><img src={user.avatar}/></td>
+              </tr>
+            )
+          })
+        ) : (
+          <tr>
+            <td>No data found</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+    </diV>
   )
 }
 
